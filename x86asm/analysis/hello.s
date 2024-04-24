defd601a1acfc6146e386efd4aa1631ba266115c
showlight <showlight@qq.com>
2024-04-24 18:45:41 +0800

    C 语言汇编分析

---
+++ x86asm/analysis/hello.s
@@ -0,0 +1,41 @@
+	.file	"hello.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"hello world!!!"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+	leal	4(%esp), %ecx
+	andl	$-16, %esp
+	pushl	-4(%ecx)
+	pushl	%ebp
+	movl	%esp, %ebp
+	pushl	%ebx
+	pushl	%ecx
+	call	__x86.get_pc_thunk.ax
+	addl	$_GLOBAL_OFFSET_TABLE_, %eax
+	subl	$12, %esp
+	leal	.LC0@GOTOFF(%eax), %edx
+	pushl	%edx
+	movl	%eax, %ebx
+	call	puts@PLT
+	addl	$16, %esp
+	movl	$0, %eax
+	leal	-8(%ebp), %esp
+	popl	%ecx
+	popl	%ebx
+	popl	%ebp
+	leal	-4(%ecx), %esp
+	ret
+	.size	main, .-main
+	.section	.text.__x86.get_pc_thunk.ax,"axG",@progbits,__x86.get_pc_thunk.ax,comdat
+	.globl	__x86.get_pc_thunk.ax
+	.hidden	__x86.get_pc_thunk.ax
+	.type	__x86.get_pc_thunk.ax, @function
+__x86.get_pc_thunk.ax:
+	movl	(%esp), %eax
+	ret
+	.ident	"GCC: (GNU) 13.2.1 20230801"
+	.section	.note.GNU-stack,"",@progbits
