d6f82fbbc380c71c6580eab3d27431adbe3a6097
showlight <showlight@qq.com>
2024-04-24 20:46:45 +0800

    compare hello.s

--- x86asm/analysis/hello.s
+++ x86asm/analysis/hello.s
@@ -3,39 +3,23 @@
 	.section	.rodata
 .LC0:
 	.string	"hello world!!!"
+.LC1:
+	.string	"hello puts!!!"
 	.text
 	.globl	main
 	.type	main, @function
 main:
-	leal	4(%esp), %ecx
-	andl	$-16, %esp
-	pushl	-4(%ecx)
 	pushl	%ebp
 	movl	%esp, %ebp
-	pushl	%ebx
-	pushl	%ecx
-	call	__x86.get_pc_thunk.ax
-	addl	$_GLOBAL_OFFSET_TABLE_, %eax
-	subl	$12, %esp
-	leal	.LC0@GOTOFF(%eax), %edx
-	pushl	%edx
-	movl	%eax, %ebx
-	call	puts@PLT
-	addl	$16, %esp
+	pushl	$.LC0
+	call	puts
+	addl	$4, %esp
+	pushl	$.LC1
+	call	puts
+	addl	$4, %esp
 	movl	$0, %eax
-	leal	-8(%ebp), %esp
-	popl	%ecx
-	popl	%ebx
-	popl	%ebp
-	leal	-4(%ecx), %esp
+	leave
 	ret
 	.size	main, .-main
-	.section	.text.__x86.get_pc_thunk.ax,"axG",@progbits,__x86.get_pc_thunk.ax,comdat
-	.globl	__x86.get_pc_thunk.ax
-	.hidden	__x86.get_pc_thunk.ax
-	.type	__x86.get_pc_thunk.ax, @function
-__x86.get_pc_thunk.ax:
-	movl	(%esp), %eax
-	ret
 	.ident	"GCC: (GNU) 13.2.1 20230801"
 	.section	.note.GNU-stack,"",@progbits
