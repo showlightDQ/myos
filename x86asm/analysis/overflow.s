80132439665584aa32db20c2053decace3da19aa
showlight <showlight@qq.com>
2024-04-27 11:54:55 +0800

    数组溢出测试  完成	movl	-4(%ebp), %edx 	subl	%gs:20, %edx 	je	.L3 	call	__stack_chk_fail

--- x86asm/analysis/overflow.s
+++ x86asm/analysis/overflow.s
@@ -1,25 +1,31 @@
 	.file	"overflow.c"
 	.text
+	.section	.rodata
+.LC0:
+	.string	"hello overflow!!!!\n"
+	.text
 	.globl	main
 	.type	main, @function
 main:
 	pushl	%ebp
 	movl	%esp, %ebp
-	subl	$8, %esp
-	leal	3(%esp), %eax
-	movdqa	.LC0, %xmm0
-	movups	%xmm0, (%eax)
-	movl	$663841, 16(%eax)
+	subl	$12, %esp
+	movl	%gs:20, %eax
+	movl	%eax, -4(%ebp)
+	xorl	%eax, %eax
+	pushl	$.LC0
+	leal	-9(%ebp), %eax
+	pushl	%eax
+	call	strcpy
+	addl	$8, %esp
 	movl	$0, %eax
+	movl	-4(%ebp), %edx
+	subl	%gs:20, %edx
+	je	.L3
+	call	__stack_chk_fail
+.L3:
 	leave
 	ret
 	.size	main, .-main
-	.section	.rodata
-	.align 16
-.LC0:
-	.long	1819043176
-	.long	1986994287
-	.long	1818653285
-	.long	555841391
 	.ident	"GCC: (GNU) 13.2.1 20230801"
 	.section	.note.GNU-stack,"",@progbits
