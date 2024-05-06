61230e12c48d54db38a48e9f93c5c88193e5b2ae
showlight <showlight@qq.com>
2024-05-06 09:25:34 +0800

    完结。

---
+++ x86asm/analysis/macro.asm
@@ -0,0 +1,9 @@
+;  执行命令 ：nasm -E macro.asm -o macro.e
+%include"macro.inc"
+%include"macro.inc"
+%include"macro.inc"
+%include"macro.inc"
+%include"macro.inc"
+mov ax,bx;
+
+call printf
---
+++ x86asm/analysis/macro.e
@@ -0,0 +1,8 @@
+%line 5+1 macro.inc
+[extern printf]
+
+
+%line 7+1 macro.asm
+mov ax,bx
+
+call printf
