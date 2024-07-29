56c5faf76b58a908cb9424af80c338407902836b
showlight <showlight@qq.com>
2024-07-30 02:41:35 +0800

    Pause

--- ONIX/src/include/onix/types.h
+++ ONIX/src/include/onix/types.h
@@ -1,26 +1,26 @@
 #ifndef ONIX_TYPES_H
-#define ONIx_TYPES_H
+    #define ONIx_TYPES_H
 
-#define EOF -1
-#define EOS '\0'
-#define NULL ((void*)0)
-#define bool _Bool 
-#define true 1
-#define false 0
-#define _packed __attribute__((packed))
-#define _ofp __attribute__((optimize("omit-frame-pointer")))
+    #define EOF -1
+    #define EOS '\0'
+    #define NULL ((void*)0)
+    #define bool _Bool 
+    #define true 1
+    #define false 0
+    #define _packed __attribute__((packed))
+    #define _ofp __attribute__((optimize("omit-frame-pointer")))
 
 
-typedef unsigned int size_t;
-  
-typedef char int8 ;
-typedef short int16 ;
-typedef int    int32 ;
-typedef long long int64 ;
+    typedef unsigned int size_t;
+    
+    typedef char int8 ;
+    typedef short int16 ;
+    typedef int    int32 ;
+    typedef long long int64 ;
 
-typedef unsigned char u8 ;
-typedef unsigned short u16 ;
-typedef unsigned int   u32 ;
-typedef unsigned long long u64 ;
+    typedef unsigned char u8 ;
+    typedef unsigned short u16 ;
+    typedef unsigned int   u32 ;
+    typedef unsigned long long u64 ;
 
 #endif
